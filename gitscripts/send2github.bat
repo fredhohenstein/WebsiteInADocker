2240c74fc2f0f7bb4dd5e70f68d94fc9f86a71ba
Fred Hohenstein <fredhohenstein@users.noreply.github.com>
2021-08-27 12:35:53 +0200

    opschonen
    opschonen

--- gitscripts/send2github.bat
+++
@@ -1,15 +0,0 @@
-@echo off
-cls
-
-cd ..
-dir
-pause
-
-git add .
-git commit -m "Weer een commit"rem git remote add origin https://github.com/fredhohenstein/WebsiteInADocker.git
-git push -u origin masterpause
-
-cd 
-cd gitscripts
-
-
